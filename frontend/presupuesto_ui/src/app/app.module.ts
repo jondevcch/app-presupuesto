6dbb1a3f8bd4b61f07d0d3fbd736ee04b7faa731
javierisp <javierisp@hotmail.com>
2022-12-19 17:08:15 -0600

    feat: cambios en los importes angular materials

--- frontend/presupuesto_ui/src/app/app.module.ts
+++ frontend/presupuesto_ui/src/app/app.module.ts
@@ -1,30 +1,49 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/structure/header/header.component';
 import { FooterComponent } from './components/structure/footer/footer.component';
 import { PresupuestoComponent } from './components/presupuesto/presupuesto/presupuesto.component';
 import { ListaPresupuestoComponent } from './components/presupuesto/lista-presupuesto/lista-presupuesto.component';
 import { MantPresupuestoComponent } from './components/presupuesto/mant-presupuesto/mant-presupuesto.component';
+import { SideNavListComponent } from './components/structure/header/sideNavList/sideNavList.component';
 import { MaterialModule } from './material.module';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'first', component: ListaPresupuestoComponent, data: { title: 'First Component' } },
+  { path: 'segundo', component: FooterComponent, data: { title: 'segundo Component' } },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    SideNavListComponent,
     HeaderComponent,
     FooterComponent,
     PresupuestoComponent,
     ListaPresupuestoComponent,
-    MantPresupuestoComponent 
+    MantPresupuestoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserModule, 
+    BrowserAnimationsModule ,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true }
+    ),
     MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
+
--- frontend/presupuesto_ui/src/app/material.module.ts
+++ frontend/presupuesto_ui/src/app/material.module.ts
@@ -1,23 +1,53 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule  } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
-  exports: [
-    MatToolbarModule,
+  imports: [
+    CommonModule,
     MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
     MatCardModule,
     MatListModule,
     MatTableModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule, 
+    MatIconModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule
+  ],
+  exports: [
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatIconModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule
   ],
 })
 export class MaterialModule {}
